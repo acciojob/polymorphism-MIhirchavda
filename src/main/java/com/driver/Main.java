988fad8cffdfa88dcbf4c0db04ad4a5cbb16db91
MIhirchavda <mihirchavda1362@gmail.com>
2022-10-09 12:19:28 +0530

    upadte code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,23 +5,23 @@ public class Main {
 
     public static class Product{
         public int product(int x , int y){
-            return x+y;
+            return x*y;
         }
         public  int product(int x , int y , int z){
-            return x+y+z;
+            return x*y*z;
         }
         public  double product(double x , double y){
-            return x+y;
+            return x*y;
         }
     }
     public static void main(String arg[]){
         Product p = new Product();
         int ans1 = p.product(10 , 15);
-        System.out.println(ans1);
+
         int ans2 = p.product(10 , 15 , 20);
-        System.out.println(ans2);
+
         double ans3 =  p.product(1.6 , 1.5);
-        System.out.println(ans3);
+        System.out.println(" ans1 "+ans1+ " ans2 " + ans2 +" ans3 "+ ans3);
 
     }
 }
